ab68dee894cd4d3a17da7b70d796cb7ace601a38
billvog <billvog04@gmail.com>
2023-08-30 15:56:12 +0300

    Display logo on place component and animate on hover

--- resources/views/components/place.blade.php
+++ resources/views/components/place.blade.php
@@ -1,10 +1,18 @@
 @props(['place'])
 
-<div {{ $attributes->merge(['class' => 'flex flex-col bg-orange-50 rounded-xl']) }}>
-  <div class="flex-1">
-    <img src="{{ $place->medially->first()?->file_url ?? null }}" class="rounded-t-xl h-full bg-black">
+<div {{ $attributes->merge(['class' => 'group flex flex-col bg-orange-50 rounded-xl']) }}>
+  <div class="flex-1 relative">
+    {{-- First photo --}}
+    <div
+      class="rounded-t-xl h-[200px] object-cover bg-black bg-center bg-[length:110%] group-hover:bg-[length:115%] transition-all"
+      style='background-image: url({{ $place->medially->first()?->file_url ?? null }})'>
+    </div>
+    {{-- Logo --}}
+    <img src="{{ $place->logo()->exists() ? $place->logo->getSecurePath() : 'https://placehold.co/100x100' }}"
+      alt='Logo for "{{ $place->name }}"'
+      class="w-16 h-16 group-hover:scale-110 transition-transform object-cover rounded-full absolute left-4 -bottom-4 border-2 border-orange-50">
   </div>
-  <div class="px-4 py-3">
+  <div class="px-4 pt-5 pb-3">
     <div class="text-base font-bold">
       <a href="{{ route('places.show', $place) }}">{{ $place->name }}</a>
     </div>
