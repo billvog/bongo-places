01510b6e489e261061b3790bd36d45aaca83674a
billvog <billvog04@gmail.com>
2023-09-03 19:58:44 +0300

    Display `place status` in the place component

--- resources/views/components/place.blade.php
+++ resources/views/components/place.blade.php
@@ -4,10 +4,19 @@
   <div class="flex-1 relative">
     {{-- Photos carousel --}}
     <x-swiper-carousel :images="$place->photos->medially" :zoomOnHover="true" class="h-[200px] rounded-t-xl"></x-swiper-carousel>
+
     {{-- Logo --}}
     <img src="{{ $place->hasLogo() ? $place->logo->getSecureUrl() : 'https://placehold.co/100x100' }}"
       alt='Logo for "{{ $place->name }}"'
       class="w-16 h-16 z-10 group-hover:scale-110 transition-transform object-cover rounded-full absolute left-4 -bottom-4 border-2 border-orange-50">
+
+    @if (Auth::check() && Auth::user()->id === $place->owner_id)
+      {{-- Post status, if we are the owner --}}
+      <div
+        class="absolute z-10 top-2 left-2 px-2.5 py-0.5 text-sm font-bold text-orange-400 bg-orange-100 bg-opacity-20 backdrop-filter backdrop-blur rounded-full">
+        {{ $place->status }}
+      </div>
+    @endif
   </div>
   <div class="px-4 pt-5 pb-3">
     <div class="text-base font-bold">
