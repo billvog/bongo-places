010b7a2f52c798a9e1026d093a3ba3622e05d983
billvog <billvog04@gmail.com>
2023-10-02 15:28:04 +0300

    Disable Vite for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,12 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
-{
-    use CreatesApplication;
+abstract class TestCase extends BaseTestCase {
+	use CreatesApplication;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->withoutVite();
+	}
 }
